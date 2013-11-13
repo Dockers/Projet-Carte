d38d4decef2c64a357d47d2fb5539b692e8bc614
Dockers <glogowski.victor@gmail.com>
2013-11-13 17:24:49 +0100

    ajout api FB connect en JS

--- FB connect/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-if(isset($_SESSION['user']['id'])):
-?>
-Bonjour, <?php echo $_SESSION['user']['login']; ?>
-<?php else : ?>
-<a href="connect.php">Se connecter avec Facebook</a>
-<?php endif; ?>
